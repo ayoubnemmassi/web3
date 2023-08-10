841516f8ee4ae9a471f5fe39dd5ad2dcaf9c54f7
Nemmassi <ayoub@feerik.com>
2023-08-10 15:34:46 +0200

    chnage entry point

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.entry = { main: ['./app/auction.js'] };
+      }
+      return config;
+    },
+  };
+  
+  module.exports = nextConfig;
+  
